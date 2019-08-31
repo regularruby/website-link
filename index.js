f8b474e6bd1ac77ee48d673bb2dd1c07245c6a4c
BuildTools <unconfigured@null.spigotmc.org>
2019-08-31 16:08:27 +0930

    add colors, fs and facebook

---
+++ index.js
@@ -0,0 +1,6 @@
+const facebook = require('facebook-chat-api')
+const Discord = require('discord.js');
+const colors = require('colors');
+const fs = require('fs');
+const client = new Discord.Client();
+
