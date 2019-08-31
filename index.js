62cae9c1a320e18a25a738671a72f4d4d26306ac
BuildTools <unconfigured@null.spigotmc.org>
2019-08-31 16:17:38 +0930

    set up

--- index.js
+++ index.js
@@ -3,4 +3,32 @@ const Discord = require('discord.js');
 const colors = require('colors');
 const fs = require('fs');
 const client = new Discord.Client();
+const login = require("./login.json")
 
+
+client.login(json.login.discord_bot.client_token);
+
+client.on("ready", () => {
+    
+});
+
+client.on("message", msg => {
+
+});
+
+messenger({
+    email: `${login.messenger.email}`,
+    password: `${login.messenger.password}`
+}, (err, fb) => {
+    if (err) return console.error(err);
+    fb.setOptions({
+        logLevel: "silent"
+    })
+    fb.listen((err, message) => {
+        if (err) return console.error(err);
+        fb.getUserInfo([message.senderID], (err, userinfo) => {
+            if (err) return console.error(err);
+            require(`./events/messenger_message.js`)(fb, message, userinfo)
+        });
+    });
+});
