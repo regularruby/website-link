397c0de1a092318cde3bc8b8ba434687a75926cf
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 01:37:31 +0930

    end

--- index.js
+++ index.js
@@ -23,20 +23,32 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-io.sockets.on('connection', (socket) => {
+io.sockets.on('connection', socket => {
     connections.push(socket);
     console.log('connected: %s sockets connected', connections.length)
 
 
-    socket.on('disconnect', (data) => {
+    socket.on('disconnect', data => {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log("disconnected: %s sockets connected", connections.length)
     })
 
-    socket.on('send message', data => {
-        io.sockets.emit('new message', {msg: data})
-        console.log(data)
+    socket.on('send message', (data, user) => {
+        io.sockets.emit('new message', {msg: data, sender: user})
     })
+
+    socket.on('new user', (data, callback) => {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username)
+        updateUsernames();
+    })
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users)
+    }
 })
 
 function discord() {
