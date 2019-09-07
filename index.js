c2fd9adba548e9d734dd76880e39746d9669e09c
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 00:02:15 +0930

    update

--- index.js
+++ index.js
@@ -1,35 +1,72 @@
 const facebook = require('facebook-chat-api')
 const Discord = require('discord.js');
-const colors = require('colors');
-const fs = require('fs');
 const client = new Discord.Client();
 const login = require("./login.json")
 
+const colors = require('colors');
+const fs = require('fs');
 
-client.login(json.login.discord_bot.client_token);
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
 
-client.on("ready", () => {
-    delete require.cache[require.resolve('./events/ready.js')]
-    require('./events/ready.js')(client,"discord")
-});
+var users = [];
+var connections = [];
 
-client.on("message", msg => {
 
-});
+//opens html
+server.listen(process.env.PORT || 3000);
+console.log('server running...')
 
-messenger({
-    email: `${login.messenger.email}`,
-    password: `${login.messenger.password}`
-}, (err, fb) => {
-    if (err) return console.error(err);
-    fb.setOptions({
-        logLevel: "silent"
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log('connected: %s sockets connected', connections.length)
+
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("disconnected: %s sockets connected", connections.length)
     })
-    fb.listen((err, message) => {
+
+    socket.on('send message', data => {
+        io.sockets.emit('new message', {msg: data})
+        console.log(data)
+    })
+})
+
+function discord() {
+    client.login(json.login.discord_bot.client_token);
+
+    client.on("ready", () => {
+        delete require.cache[require.resolve('./events/ready.js')]
+        require('./events/ready.js')(client, "discord")
+    });
+
+    client.on("message", msg => {
+
+    });
+}
+
+function messenger() {
+    messenger({
+        email: `${login.messenger.email}`,
+        password: `${login.messenger.password}`
+    }, (err, fb) => {
         if (err) return console.error(err);
-        fb.getUserInfo([message.senderID], (err, userinfo) => {
+        fb.setOptions({
+            logLevel: "silent"
+        })
+        fb.listen((err, message) => {
             if (err) return console.error(err);
-            require(`./events/messenger_message.js`)(fb, message, userinfo)
+            fb.getUserInfo([message.senderID], (err, userinfo) => {
+                if (err) return console.error(err);
+                require(`./events/messenger_message.js`)(fb, message, userinfo)
+            });
         });
     });
-});
+}
